3a00e41276ada60ad1842b243d0ab0de7822f88e
Kyle Shenk <kyle.shenk@jazz.co>
2016-11-10 14:08:21 -0500

    Moved filtering for states to the worker, deprecating --running in favor of a new arg where the user can supply the state to filter on

--- supermgr/worker.py
+++ supermgr/worker.py
@@ -13,7 +13,16 @@ class Worker(Server):
         if not isinstance(self.server, xmlrpclib.Server):
             sys.exit(2)
 
-    def get_workers(self, group_names=None):
+    def __filter_workers(self, worker, filter=None):
+        if filter is not None:
+            if worker.get('statename') == filter:
+                return {worker.get('name'): worker}
+        else:
+            return {worker.get('name'): worker}
+
+        return None
+
+    def get_workers(self, group_names=None, filter_state=None):
         data    = self.server.supervisor.getAllProcessInfo()
         workers = defaultdict(dict)
 
@@ -23,11 +32,13 @@ class Worker(Server):
         for info in data:
             if group_names:
                 if info.get('group') in group_names:
-                    _data = {info.get('name'): info}
-                    workers[info.get('group')].update(_data)
+                    _data = self.__filter_workers(info, filter_state)
+                    if _data:
+                        workers[info.get('group')].update(_data)
             else:
-                _data = {info.get('name'): info}
-                workers[info.get('group')].update(_data)
+                _data = self.__filter_workers(info, filter_state)
+                if _data:
+                    workers[info.get('group')].update(_data)
 
         for n, d in workers.items():
             workers[n] = OrderedDict(sorted(d.items(), key=lambda k: k[0]))
